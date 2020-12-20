5c39b4e732b257749d3a87723ff3bc12a74a31ec
Brian Chow <32953270+khc25@users.noreply.github.com>
2020-12-20 19:34:10 +0800

    fix connection url

--- app.py
+++ app.py
@@ -2,8 +2,19 @@ from flask import Flask
 app = Flask(__name__)
 from flask import request
 import psycopg2
+import urlparse
 
-conn = psycopg2.connect("dbpostgres://obbyumtp:McdTVFblMMvaFMEgp9FaC44jS7j7ZwX1@suleiman.db.elephantsql.com:5432/obbyumtp")
+result = urlparse.urlparse("dbpostgres://obbyumtp:McdTVFblMMvaFMEgp9FaC44jS7j7ZwX1@suleiman.db.elephantsql.com:5432/obbyumtp")
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+conn = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname
+)
 cur = conn.cursor()
 
 @app.route('/post', methods=["POST"])
--- createTable.py
+++ createTable.py
@@ -1,6 +1,18 @@
 import psycopg2
 
-conn = psycopg2.connect("dbpostgres://obbyumtp:McdTVFblMMvaFMEgp9FaC44jS7j7ZwX1@suleiman.db.elephantsql.com:5432/obbyumtp")
+import urlparse
+
+result = urlparse.urlparse("dbpostgres://obbyumtp:McdTVFblMMvaFMEgp9FaC44jS7j7ZwX1@suleiman.db.elephantsql.com:5432/obbyumtp")
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+conn = psycopg2.connect(
+    database = database,
+    user = username,
+    password = password,
+    host = hostname
+)
 cur = conn.cursor()
 
 cur.execute("CREATE TABLE test (id serial PRIMARY KEY, usernmae varchar, password varchar);")
